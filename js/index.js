8225cf206eeb7e502c5f658e00286c531334d9e0
Mahmud <rainbowmahmud@gmail.com>
2022-03-01 10:13:42 +0600

    phone detials added.

--- js/index.js
+++ js/index.js
@@ -93,6 +93,40 @@ const apiRes = {
     },
   ],
 };
+
+const apiResPhone = {
+  status: true,
+  data: {
+    mainFeatures: {
+      storage: "128GB/256GB/1TB storage, no card slot",
+      displaySize: "6.7 inches, 109.8 cm2 (~87.4% screen-to-body ratio)",
+      chipSet: "Apple A15 Bionic (5 nm)",
+      memory: "128GB 6GB RAM, 256GB 6GB RAM, 512GB 6GB RAM, 1TB 6GB RAM",
+      sensors: [
+        "Face ID",
+        "accelerometer",
+        "gyro",
+        "proximity",
+        "compass",
+        "barometer",
+      ],
+    },
+    slug: "apple_iphone_13_pro_max-11089",
+    name: "iPhone 13 Pro Max",
+    releaseDate: "Released 2021, September 24",
+    brand: "Apple",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro-max.jpg",
+    others: {
+      WLAN: "Wi-Fi 802.11 a/b/g/n/ac/6, dual-band, hotspot",
+      Bluetooth: "5.0, A2DP, LE",
+      GPS: "Yes, with A-GPS, GLONASS, GALILEO, BDS, QZSS",
+      NFC: "Yes",
+      Radio: "No",
+      USB: "Lightning, USB 2.0",
+    },
+  },
+};
+
 // URL
 const URL = "https://openapi.programming-hero.com/api/";
 
@@ -106,29 +140,77 @@ const setDOMValue = (selector, content, method = "innerHTML") => {
   document.querySelector(selector)[method] = content;
 };
 
+// show main features
+// const features = () => {
+//   const html = ""
+
+// }
+
 // Display Phones
 const displayPhones = (phones, node) => {
   // const display = getDOMValue();
   // console.log(phones);
-
-  const phoneItems = phones.map(({ brand, phone_name, slug, image }) => {
-    return `
+  if (Array.isArray(phones)) {
+    const phoneItems = phones.map(({ brand, phone_name, slug, image }) => {
+      return `
     <div class="col phone">
       <div class="card">
         <div class="card-body">
         <img src="${image}" class="mb-4" alt="${phone_name}">
             <h6 class="text-secondary mb-0">${brand}</h6>
             <h5 class="card-title text-primary mb-3">${phone_name}</h5>
-            <button slug="${slug}" class="btn btn-success inspect-button">Inspect</button>
+            <button onclick="loadPhones(
+              '${slug}', 'phone/'
+            )" class="btn btn-success inspect-button">Inspect</button>
           </div>
         </div>
       </div>
     `;
-  });
+    });
+    node.innerHTML = `<h2 class="col-md-12 pt-5 text-center">Phones</h2>${phoneItems.join(
+      ""
+    )}`;
+  } else {
+    const features = Object.entries(phones);
+    const { mainFeatures, name, brand, image, others } = phones;
+    const { storage, displaySize, chipSet, memory } = mainFeatures;
 
-  node.innerHTML = `<h2 class="col-md-12 pt-5 text-center">Phones</h2>${phoneItems.join(
-    ""
-  )}`;
+    const phoneCard = `
+      <div class="col col-md-12">
+          Phone Details
+        </div>
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+            <img src="${image}" class="mb-4" alt="${name}">
+                <h6 class="text-secondary mb-0">${brand}</h6>
+                <h5 class="card-title text-primary mb-0">${phone_name}</h5>
+                <small class="card-title text-primary mb-3">${phones?.releaseDate}</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Storage: ${storage}</li>
+                <li class="list-group-item">Display Size: ${displaySize}</li>
+                <li class="list-group-item">Chip Set: ${chipSet}</li>
+                <li class="list-group-item">Memory: ${memory}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+    `;
+    // console.log(features);
+    // node.innerHTML = `<h2 class="col-md-12 pt-5 text-center">Phones</h2>${phoneItems.join(
+    //   ""
+    // )}`;
+  }
 };
 
 // Load phones
@@ -150,12 +232,15 @@ const loadPhones = async (slug, term = "phones?search=") => {
 document.querySelector(".search-form").addEventListener("submit", (e) => {
   // prevent default submit
   e.preventDefault();
-  e.stopImmediatePropagation();
 
   // search input
   const inputSearch = getDOMValue(".phone-search-input", "value");
 
   // load data
   // loadPhones(inputSearch);
-  displayPhones(apiRes.data, document.querySelector(".phones-display"));
+  // displayPhones(
+  //   apiRes.data.slice(0, 20),
+  //   document.querySelector(".phones-display")
+  // );
+  displayPhones(apiResPhone.data, document.querySelector(".phone-details"));
 });
