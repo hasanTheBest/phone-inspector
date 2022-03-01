5b0e2088a2911dce3cbfb5103037d2bcdb5602a4
Mahmud <rainbowmahmud@gmail.com>
2022-03-01 11:24:29 +0600

    almost finished. need to add spinner

--- js/index.js
+++ js/index.js
@@ -1,132 +1,3 @@
-const apiRes = {
-  status: true,
-  data: [
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 13 mini",
-      slug: "apple_iphone_13_mini-11104",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-mini.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 13 Pro",
-      slug: "apple_iphone_13_pro-11102",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 13 Pro Max",
-      slug: "apple_iphone_13_pro_max-11089",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro-max.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 13",
-      slug: "apple_iphone_13-11103",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 12 Pro Max",
-      slug: "apple_iphone_12_pro_max-10237",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-pro-max-.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 12 Pro",
-      slug: "apple_iphone_12_pro-10508",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-pro--.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 12",
-      slug: "apple_iphone_12-10509",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 12 mini",
-      slug: "apple_iphone_12_mini-10510",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-mini.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone SE (2020)",
-      slug: "apple_iphone_se_(2020)-10170",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-se-2020.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 11 Pro Max",
-      slug: "apple_iphone_11_pro_max-9846",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-11-pro.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 11 Pro",
-      slug: "apple_iphone_11_pro-9847",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-11-pro-max-.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 11",
-      slug: "apple_iphone_11-9848",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-11.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone XS Max",
-      slug: "apple_iphone_xs_max-9319",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-xs-max-new1.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone XR",
-      slug: "apple_iphone_xr-9320",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-xr-new.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone XS",
-      slug: "apple_iphone_xs-9318",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-xs-new.jpg",
-    },
-  ],
-};
-
-const apiResPhone = {
-  status: true,
-  data: {
-    mainFeatures: {
-      storage: "128GB/256GB/1TB storage, no card slot",
-      displaySize: "6.7 inches, 109.8 cm2 (~87.4% screen-to-body ratio)",
-      chipSet: "Apple A15 Bionic (5 nm)",
-      memory: "128GB 6GB RAM, 256GB 6GB RAM, 512GB 6GB RAM, 1TB 6GB RAM",
-      sensors: [
-        "Face ID",
-        "accelerometer",
-        "gyro",
-        "proximity",
-        "compass",
-        "barometer",
-      ],
-    },
-    slug: "apple_iphone_13_pro_max-11089",
-    name: "iPhone 13 Pro Max",
-    releaseDate: "Released 2021, September 24",
-    brand: "Apple",
-    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro-max.jpg",
-    others: {
-      WLAN: "Wi-Fi 802.11 a/b/g/n/ac/6, dual-band, hotspot",
-      Bluetooth: "5.0, A2DP, LE",
-      GPS: "Yes, with A-GPS, GLONASS, GALILEO, BDS, QZSS",
-      NFC: "Yes",
-      Radio: "No",
-      USB: "Lightning, USB 2.0",
-    },
-  },
-};
-
 // URL
 const URL = "https://openapi.programming-hero.com/api/";
 
@@ -162,10 +33,17 @@ const ShowFeatures = (features, title) => {
 };
 
 // Display Phones
-const displayPhones = (phones, node) => {
+const displayPhones = (phones, node, err = false) => {
+  if (err === true) {
+    node.innerHTML = phones;
+    return;
+  }
+
   if (Array.isArray(phones)) {
-    const phoneItems = phones.map(({ brand, phone_name, slug, image }) => {
-      return `
+    const phoneItems = phones
+      .slice(0, 20)
+      .map(({ brand, phone_name, slug, image }) => {
+        return `
     <div class="col phone">
       <div class="card">
         <div class="card-body">
@@ -179,24 +57,24 @@ const displayPhones = (phones, node) => {
         </div>
       </div>
     `;
-    });
-    node.innerHTML = `<h2 class="col-md-12 pt-5 text-center">Phones</h2>${phoneItems.join(
+      });
+    node.innerHTML = `<h2 class="col-12 col-md-12 pt-5 text-center">Phones</h2>${phoneItems.join(
       ""
     )}`;
   } else {
     const { mainFeatures, name, brand, image, others } = phones;
 
     const phoneCard = `
-      <div class="col col-md-12">
+      <div class="col-12 col-md-12">
           <h2 class="text-center my-5">Phone Details</h2>
         </div>
         <div class="col">
           <div class="card">
             <div class="card-body">
             <img src="${image}" class="mb-4" alt="${name}">
-                <h6 class="text-secondary mb-0">${brand}</h6>
-                <h5 class="card-title text-primary mb-0">${name}</h5>
-                <small class="card-title text-primary mb-3">${
+                <h6 class="text-secondary mb-0 text-uppercase">${brand}</h6>
+                <h5 class="card-title text-primary mb-0 text-capitalized">${name}</h5>
+                <small class="card-title text-secondary mb-3 text-capitalized">${
                   phones?.releaseDate
                 }</small>
               </div>
@@ -214,16 +92,33 @@ const displayPhones = (phones, node) => {
 
 // Load phones
 const loadPhones = async (slug, term = "phones?search=") => {
+  // show error
+  const node =
+    term === "phones?search="
+      ? document.querySelector(".phones-display")
+      : document.querySelector(".phone-details");
+
   try {
     const response = await fetch(URL + term + slug.toLowerCase());
     const data = await response.json();
 
     // Display Phones
+
     if (data.status) {
-      displayPhones(data.data);
+      displayPhones(data.data, node);
+    } else {
+      displayPhones(
+        "<h1 class='col-md-12 text-center text-danger py-5 px-3'>Not Found</h1>",
+        node,
+        true
+      );
     }
   } catch (err) {
-    console.log(err.message);
+    displayPhones(
+      `<h1 class='col-md-12 text-center text-danger py-5 px-3'>${err.message}</h1>`,
+      node,
+      true
+    );
   }
 };
 
@@ -236,10 +131,8 @@ document.querySelector(".search-form").addEventListener("submit", (e) => {
   const inputSearch = getDOMValue(".phone-search-input", "value");
 
   // load data
-  // loadPhones(inputSearch);
-  // displayPhones(
-  //   apiRes.data.slice(0, 20),
-  //   document.querySelector(".phones-display")
-  // );
-  displayPhones(apiResPhone.data, document.querySelector(".phone-details"));
+  loadPhones(inputSearch);
+
+  setDOMValue(".phone-details", "");
+  setDOMValue(".phones-display", "");
 });
